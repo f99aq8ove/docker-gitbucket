bee407e054c477f82449a8dfaabbc52ad6ae7e35
in_dow <in_dow@hotmail.com>
2017-11-14 00:48:32 +0900

    Embed a certificate to container
    Signed-off-by: in_dow <in_dow@hotmail.com>

--- gitbucket.sh
+++ gitbucket.sh
@@ -10,4 +10,21 @@ db {
 EOCONF
 fi
 
+if [ "$GITBUCKET_CERT" ]; then
+  TARGET_TLS_PATH=$JAVA_HOME/lib/security/cacerts
+  # another container may creates cert
+  timeout=0
+  while [ ! -f $GITBUCKET_CERT -a $timeout -lt 60 ]
+  do
+    timeout=`expr $timeout + 1`
+    sleep 1
+    echo Waiting for $GITBUCKET_CERT ....
+  done
+  if [ -f $GITBUCKET_CERT ]; then
+    echo Getting ready $GITBUCKET_CERT to $TARGET_TLS_PATH
+    echo yes | keytool -import -trustcacerts -storepass 'changeit' -alias sambacert -file $GITBUCKET_CERT -keystore $TARGET_TLS_PATH
+    keytool -list -storepass 'changeit' -keystore $TARGET_TLS_PATH
+  fi
+fi
+
 exec java $JAVA_OPTS -jar /opt/gitbucket.war $GITBUCKET_OPTS
